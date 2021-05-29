5ee4aeba2f82e6d9ea0ec69a62b6a52efd57e6e3
H388kk <78017274+H388kk@users.noreply.github.com>
2021-05-29 13:19:57 +0300

    Popup strada

---
+++ JS/admin.js
@@ -0,0 +1,8 @@
+document.getElementById("pievienot_celojumu").addEventListener("click", () => {
+    console.log("Poga nospiesta")
+
+    let popup = document.getElementById("popup");
+    popup.style.display = "block"
+
+
+})
