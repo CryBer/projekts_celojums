0c7037f857205fbf9c90bb6479d75f01576a0501
H388kk <78017274+H388kk@users.noreply.github.com>
2021-05-29 15:05:39 +0300

    javaskript

--- JS/admin.js
+++ JS/admin.js
@@ -1,8 +1,26 @@
+const POP_UP = document.getElementById("popup");
+/*vairāki celojumi*/
+let celojumi = [];
+
 document.getElementById("pievienot_celojumu").addEventListener("click", () => {
-    console.log("Poga nospiesta")
+    POP_UP.style.display = "block";
+})
 
-    let popup = document.getElementById("popup");
-    popup.style.display = "block"
+document.getElementById("pievienot_poga").addEventListener("click", () => {
+    POP_UP.style.display = "none";
+    console.log(popup_valsts_src.value);
+    console.log(popup_datums_src.value);
+    console.log(popup_cena_src.value);
+    console.log(popup_apraksts_src.value);
 
+    /*Viens celojums*/
+    let celojums = { valsts: popup_valsts_src.value, datums: popup_datums_src.value, cena: popup_cena_src.value, apraksts: popup_apraksts_src.value };
+    /* valsts.value = ""; */
+    console.log(celojumi);
+    celojumi.push(celojums);
 
 })
+
+function render() {
+    let bibleoteka = documents.getElementById("bibleoteka");
+}
